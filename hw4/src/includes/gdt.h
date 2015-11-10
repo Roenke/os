cddbe7c3d3a9f779f220b487a92f70abc9fa23c8
Roenke <roenke54@gmail.com>
2015-11-10 21:12:02 +0300

    init hw4 from hw2

---
+++ hw4/src/includes/gdt.h
@@ -0,0 +1,8 @@
+#ifndef GDT_H
+#define GDT_H
+
+#include "int_types.h"
+
+void init_gdt();
+
+#endif
---
+++ hw4/src/screen.c
@@ -0,0 +1,134 @@
+/* The number of columns. */
+#define COLUMNS                 80
+/* The number of lines. */
+#define LINES                   24
+/* The attribute of an character. */
+#define ATTRIBUTE               7
+/* The video memory address. */
+#define VIDEO                   0xB8000
+
+#include "includes/screen.h"
+
+static int xpos;
+static int ypos;
+static volatile unsigned char *video;
+
+static void itoa (char *buf, int base, int d)
+{
+    char *p = buf;
+    char *p1, *p2;
+    unsigned long ud = d;
+    int divisor = 10;
+
+    if (base == 'd' && d < 0)
+    {
+        *p++ = '-';
+        buf++;
+        ud = -d;
+    }
+    else if (base == 'x')
+    {
+        divisor = 16;
+    }
+
+    do
+    {
+        int remainder = ud % divisor;
+
+        *p++ = (remainder < 10) ? remainder + '0' : remainder + 'a' - 10;
+    }
+    while (ud /= divisor);
+
+    *p = 0;
+
+    p1 = buf;
+    p2 = p - 1;
+    while (p1 < p2)
+    {
+        char tmp = *p1;
+        *p1 = *p2;
+        *p2 = tmp;
+        p1++;
+        p2--;
+    }
+}
+
+static void putchar (int c)
+{
+    if (c == '\n' || c == '\r')
+    {
+        newline:
+        xpos = 0;
+        ypos++;
+        if (ypos >= LINES)
+        ypos = 0;
+        return;
+    }
+
+    *(video + (xpos + ypos * COLUMNS) * 2) = c & 0xFF;
+    *(video + (xpos + ypos * COLUMNS) * 2 + 1) = ATTRIBUTE;
+
+    xpos++;
+    if (xpos >= COLUMNS)
+    {
+        goto newline;
+    }
+}
+
+void cls (void)
+{
+    int i;
+
+    video = (unsigned char *) VIDEO;
+
+    for (i = 0; i < COLUMNS * LINES * 2; i++)
+        *(video + i) = 0;
+
+    xpos = 0;
+    ypos = 0;
+}
+
+void printf (const char *format, ...)
+{
+    char **arg = (char **) &format;
+    int c;
+    char buf[20];
+
+    arg++;
+
+    while ((c = *format++) != 0)
+    {
+        if (c != '%')
+            putchar (c);
+        else
+        {
+            char *p;
+
+            c = *format++;
+            switch (c)
+            {
+            case 'd':
+            case 'u':
+            case 'x':
+                itoa (buf, c, *((int *) arg++));
+                p = buf;
+                goto string;
+                break;
+
+            case 's':
+                p = *arg++;
+                if (! p)
+                p = "(null)";
+
+                string:
+                while (*p)
+                putchar (*p++);
+                break;
+
+            default:
+                putchar (*((int *) arg++));
+                break;
+            }
+        }
+    }
+}
